8fe1f81e9b81dce723553646577ea675274e164f
Chris Edgington <chrisedg87@me.com>
2021-01-02 17:32:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# rounded_loading_button
+
+[![pub package](https://img.shields.io/pub/v/animated_check.svg)](https://pub.dev/packages/animated_check) 
+
+AnimatedCheck is a Flutter package with a simple implementation of an animated checkmark icon.
+
+![](screenshots/check_animation.gif)
+
+## Installation
+
+   Add this to your pubspec.yaml:
+    
+    dependencies:
+        animated_check: ^1.0.0
+
+## Usage
+
+### Import
+
+    import 'package:animated_check/animated_check.dart';
+
+### Simple Implementation
+
+    AnimationController(vsync: this, duration: Duration(seconds: 1));
+    Animation _animation = new Tween<double>(begin: 0, end: 1)
+      .animate(new CurvedAnimation(
+        parent: _animationController, 
+        curve: Curves.easeInOutCirc)
+      );
+
+    void _showCheck() {
+      _animation.forward();
+    }
+
+    void _resetCheck() {
+      _animation.reverse();
+    }
+
+    AnimatedCheck(
+      progress: _animation,
+      size: 200,
+    )
+
+## Contributions
+
+All contributions are welcome!
---
+++ example/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:animated_check/animated_check.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rounded Loading Button Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Rounded Loading Button Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+
+  AnimationController _animationController;
+  Animation<double> _animation;
+
+  @override
+  void initState() { 
+    super.initState();
+    
+    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 1));
+    
+    _animation = new Tween<double>(begin: 0, end: 1).animate(new CurvedAnimation(parent: _animationController, curve: Curves.easeInOutCirc));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animated Check Example"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: AnimatedCheck(
+                progress: _animation,
+                size: 200,
+              )
+            ),
+            FlatButton(
+              child: Text("Check"),
+              onPressed: _animationController.forward),
+            FlatButton(
+              child: Text("Reset"),
+              onPressed: _animationController.reset)
+          ],
+        ),
+      )
+    );
+  }
+}
